d87f8760fe852cbb7cb904510b0555d607536328
Qaisoo12 <39465735+Qaisoo12@users.noreply.github.com>
2018-05-20 21:54:00 +0300

    Add files via upload

---
+++ welcome.java
@@ -0,0 +1,12 @@
+
+public class welcome
+{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int x=7;
+        int y=9;
+        System.out.println(x+y+6);
+        System.out.println(5);
+
+    }
+}
