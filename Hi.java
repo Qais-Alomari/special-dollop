a9555b173357f3de06632041898f386d2c44d390
Qaisoo12 <39465735+Qaisoo12@users.noreply.github.com>
2018-05-20 22:34:03 +0300

    Add files via upload

---
+++ Hi.java
@@ -0,0 +1,15 @@
+public class welcome
+{
+    public static void main(String[] args) {
+
+        int x=7;
+        int y=x-9;
+        if(x>y)
+            System.out.println(y);
+        else
+            System.out.println(x);
+
+        System.out.println("Push");
+
+    }
+}
