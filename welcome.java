d2bbc20f27d501eefd13f8b5073285d6f023cdb2
Qaisoo12 <39465735+Qaisoo12@users.noreply.github.com>
2018-05-20 21:55:28 +0300

    Update welcome.java

--- welcome.java
+++ welcome.java
@@ -5,8 +5,12 @@ public class welcome
         System.out.println("Hello World!");
         int x=7;
         int y=9;
-        System.out.println(x+y+6);
-        System.out.println(5);
+        if(x>y) 
+        System.out.println(x);
+        else 
+        System.out.println(y);
+            
+            
 
     }
 }
